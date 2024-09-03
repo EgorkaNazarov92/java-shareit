6aa36ce0b1206ecb22377ae5ae21f3947b794a84
Egor Nazarov <nazarovegorka@yandex.ru>
2024-09-03 19:37:37 +0300

    Sprint #15 - правки

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -40,7 +40,7 @@ public class Booking {
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
-		if (!(o instanceof Item)) return false;
+		if (!(o instanceof Booking)) return false;
 		return id != null && id.equals(((Booking) o).getId());
 	}
 
